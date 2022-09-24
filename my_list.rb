51785d0766cdc96d8878c6283b66333646954e9e
wdavidcch <david.13.2121@gmail.com>
2022-09-24 14:51:01 -0400

    Linterts

--- my_list.rb
+++ my_list.rb
@@ -16,22 +16,22 @@ class MyList
   end
 end
 
-# Create our list
-irb> list = MyList.new(1, 2, 3, 4)
-=> #<MyList: @list=[1, 2, 3, 4]>
+# # Create our list
+# irb> list = MyList.new(1, 2, 3, 4)
+# => #<MyList: @list=[1, 2, 3, 4]>
 
-# Tests #all?
-irb> list.all? {|e| e < 5}
-=> true
-irb> list.all? {|e| e > 5}
-=> false
+# # Tests #all?
+# irb> list.all? {|e| e < 5}
+# => true
+# irb> list.all? {|e| e > 5}
+# => false
 
-# Tests #any?
-irb> list.any? {|e| e == 2}
-=> true
-irb> list.any? {|e| e == 5}
-=> false
+# # Tests #any?
+# irb> list.any? {|e| e == 2}
+# => true
+# irb> list.any? {|e| e == 5}
+# => false
 
-# Tests #filter
-irb> list.filter {|e| e.even?}
-=> [2, 4]
+# # Tests #filter
+# irb> list.filter {|e| e.even?}
+# => [2, 4]
