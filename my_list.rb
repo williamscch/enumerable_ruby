5dd50c63d24404f1388effefeb2d950a001efebb
Vitor Guedes Madeira <vgm_rox@hotmail.com>
2022-09-24 15:48:30 -0300

    Fix tests

--- my_list.rb
+++ my_list.rb
@@ -20,18 +20,18 @@ end
 irb> list = MyList.new(1, 2, 3, 4)
 => #<MyList: @list=[1, 2, 3, 4]>
 
-# Test #all?
+# Tests #all?
 irb> list.all? {|e| e < 5}
 => true
 irb> list.all? {|e| e > 5}
 => false
 
-# Test #any?
+# Tests #any?
 irb> list.any? {|e| e == 2}
 => true
 irb> list.any? {|e| e == 5}
 => false
 
-# Test #filter
+# Tests #filter
 irb> list.filter {|e| e.even?}
 => [2, 4]
